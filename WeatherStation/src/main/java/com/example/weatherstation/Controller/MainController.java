751edbce86b25370df84fb98ee75c298d493583b
jool2102 <jool2102>
2022-05-18 13:40:47 +0200

    pog

--- WeatherStation/src/main/java/com/example/weatherstation/Controller/MainController.java
+++ WeatherStation/src/main/java/com/example/weatherstation/Controller/MainController.java
@@ -1,4 +1,23 @@
 package com.example.weatherstation.Controller;
 
-public class MainController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController
+{
+    @RequestMapping("/index")
+    public String returnIndex()
+    {
+        return "index";
+        //Länk till hemsidan för fullständig rapport
+        //Live-rapport
+    }
+
+    @RequestMapping("/report")
+    public String returnReport()
+    {
+        return "report";
+        //Full historik
+    }
 }
